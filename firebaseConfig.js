51d858b5019fa7f226d8f9537250665c98881c55
gelecek-rotasi <skanik95@gmail.com>
2026-06-08 14:28:10 +0300

    Add Firebase configuration settings

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,1 +1,9 @@
+firebaseConfig = { 
+  apiKey : "AIzaSyD_J6_tufG4nWGJ09nicpcF6fsuq1wnH34" , 
+  authDomain : "gelecek-rotasi-360.firebaseapp.com" , 
+  projectId : "gelecek-rotasi-360" , 
+  storageBucket : "gelecek-rotasi-360.firebasestorage.app" , 
+  messagingSenderId : "351598035620" , 
+  appId : "1:351598035620:web:6f64dab6740e206b335c31" 
+};
 
